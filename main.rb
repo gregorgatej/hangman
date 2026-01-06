90c6de72b871647c8108b00cdc2bb717e0db06a1
Gregor Gatej <gregor.gatej@gmail.com>
2026-01-06 05:44:43 +0100

    feat: add mask word functionality and represent it to the user

--- main.rb
+++ main.rb
@@ -3,3 +3,10 @@ fname = "./data/google-10000-english-no-swears.txt"
 word_array = File.readlines(fname)
 filtered_words = word_array.map(&:chomp).filter { |word| word.length >= 5 && word.length <= 12 }
 random_word = filtered_words.sample()
+masked_word = "_" * random_word.length
+
+masked_word_representation = Array.new(masked_word.length, "_").join(" ")
+puts "A random word has been chosen:"
+puts "Psst! The random word is #{random_word}"
+puts masked_word_representation
+
