b1db4ba60cae35718d9c15e1fc24fe0164b607df
Gregor Gatej <gregor.gatej@gmail.com>
2026-01-06 06:05:50 +0100

    feat: implement guessing logic and show succesfull guesses to the user

--- main.rb
+++ main.rb
@@ -2,11 +2,18 @@ fname = "./data/google-10000-english-no-swears.txt"
 
 word_array = File.readlines(fname)
 filtered_words = word_array.map(&:chomp).filter { |word| word.length >= 5 && word.length <= 12 }
-random_word = filtered_words.sample()
-masked_word = "_" * random_word.length
+secret_word = filtered_words.sample()
+masked_word = "_" * secret_word.length
 
-masked_word_representation = Array.new(masked_word.length, "_").join(" ")
-puts "A random word has been chosen:"
-puts "Psst! The random word is #{random_word}"
-puts masked_word_representation
+puts "A random secret word has been chosen:"
+puts "Psst! The secret word is #{secret_word}"
+puts masked_word.chars.join(" ")
 
+puts "What will be your guess?"
+guess = gets.chomp
+
+secret_word.chars.each_with_index do |letter, index| 
+  masked_word[index] = guess if letter == guess
+end
+
+puts masked_word.chars.join(" ")
